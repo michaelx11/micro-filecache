7b79aaa9f7693d9bcf76f4394db92363d340ae41
Michael Xu <michaeljxu11@gmail.com>
2015-03-04 20:25:29 -0500

    updated commands

--- node/commands.sh
+++ node/commands.sh
@@ -1,8 +1,17 @@
 mstore () {
+  if [[ $# -eq 0 ]] ; then
+    echo "Missing filepath"
+    return 0;
+  fi
   curl -F filedata=@$1 http://m.xvm.mit.edu/upload
 }
 
 mload () {
+  if [[ $# -eq 0 ]] ; then
+    echo "trying one"
+    wget --content-disposition http://m.xvm.mit.edu/current
+    return 0;
+  fi
   wget --content-disposition http://m.xvm.mit.edu/$1
 }
 
