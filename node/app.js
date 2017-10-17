197b412a866f65970bd69de16009d63ae4ab4e91
Michael Xu <michaeljxu11@gmail.com>
2017-10-16 23:35:41 -0700

    running with express 4.x

--- node/app.js
+++ node/app.js
@@ -1,27 +1,20 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var http = require('express');
 var hbs = require('hbs');
 var model = require('./model');
+var morgan = require('morgan');
 
 var app = express();
 
-app.set('port', 80);
+app.set('port', 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
-app.use(express.cookieParser());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(morgan('dev'));
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
-app.use(express.session({ secret: 'SECRET' }));
-
-app.use(app.router);
-
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', function(req, res) {
   res.render('index.html');
